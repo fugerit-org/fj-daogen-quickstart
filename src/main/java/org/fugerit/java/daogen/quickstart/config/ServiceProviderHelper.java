4734710d98e811e598c2e66de5d1516dae892763
Daneel <d@fugerit.org>
2019-08-26 08:07:36 +0200

    Finishing setup

--- src/main/java/org/fugerit/java/daogen/quickstart/config/ServiceProviderHelper.java
+++ src/main/java/org/fugerit/java/daogen/quickstart/config/ServiceProviderHelper.java
@@ -4,6 +4,8 @@ import org.fugerit.java.core.db.dao.DAOException;
 import org.fugerit.java.core.db.daogen.CloseableDAOContext;
 import org.fugerit.java.core.db.daogen.CloseableDAOContextSC;
 import org.fugerit.java.core.db.daogen.SimpleServiceProvider;
+import org.fugerit.java.daogen.quickstart.def.facade.QuickstartLogicFacade;
+import org.fugerit.java.daogen.quickstart.impl.facade.data.QuickstartDataLogicFacade;
 
 public class ServiceProviderHelper<T> extends SimpleServiceProvider<T> {
 
@@ -12,10 +14,14 @@ public class ServiceProviderHelper<T> extends SimpleServiceProvider<T> {
 	 */
 	private static final long serialVersionUID = -3421974923085310713L;
 
+	private final static QuickstartLogicFacade FACADE = new QuickstartDataLogicFacade();
+	
 	@Override
 	protected CloseableDAOContext newDefaultContext() throws DAOException {
 		try {
-			return new CloseableDAOContextSC( QuickstartDBHelper.newConnection() );
+			CloseableDAOContext context = new CloseableDAOContextSC( QuickstartDBHelper.newConnection() );
+			context.setAttribute( QuickstartLogicFacade.ATT_NAME , FACADE );
+			return context;
 		} catch (Exception e) {
 			throw new DAOException( e );
 		}
